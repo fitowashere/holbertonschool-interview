3c2c405d15161f4b5198e6291064db770a5be5ae
fitowashere <fito_washere@icloud.com>
2025-02-22 10:46:28 -0500

    Optimize cycle detection algorithm in linked list for improved readability

--- linked_list_cycle/0-check_cycle.c
+++ linked_list_cycle/0-check_cycle.c
@@ -7,23 +7,14 @@
  */
 int check_cycle(listint_t *list)
 {
-    listint_t *slow, *fast;
+    listint_t *slow = list, *fast = list;
 
-    /* If list is empty or has only one node, there can't be a cycle */
-    if (list == NULL || list->next == NULL)
-        return (0);
-
-    slow = list;      /* Moves one step at a time */
-    fast = list->next; /* Moves two steps at a time */
-
-    while (slow != NULL && fast != NULL && fast->next != NULL)
+    while (fast && fast->next)
     {
-        if (slow == fast)  /* If they meet, there is a cycle */
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
             return (1);
-        
-        slow = slow->next;         /* Move slow pointer one step */
-        fast = fast->next->next;   /* Move fast pointer two steps */
     }
-
-    return (0);  /* If we exit the loop, there is no cycle */
+    return (0);
 }
