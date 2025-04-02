533479c960ffb02deaa4abe7f7544c34d463cfe1
fitowashere <fito_washere@icloud.com>
2025-04-01 22:25:51 -0400

    Fix include guards and add stdlib.h for memory allocation in AVL tree header

--- sorted_array_to_avl/binary_trees.h
+++ sorted_array_to_avl/binary_trees.h
@@ -1,6 +1,7 @@
-#ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
+#include <stdlib.h>
 #include <stddef.h>
 
 /**
@@ -13,12 +14,16 @@
  */
 typedef struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
 } binary_tree_t;
 
-void binary_tree_print(const binary_tree_t *);
+typedef struct binary_tree_s avl_t;
 
-#endif /* _BINARY_TREES_H_ */
+void binary_tree_print(const binary_tree_t *);
+void _binary_tree_delete(binary_tree_t *tree);
+avl_t *sorted_array_to_avl(int *array, size_t size);
+
+#endif /* BINARY_TREES_H */
