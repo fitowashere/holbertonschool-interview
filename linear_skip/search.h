4ed1f7899bbaf44c45bbfbc751e47177b4406f5f
fitowashere <fito_washere@icloud.com>
2025-04-12 07:35:49 -0400

    Add function declarations for creating and freeing skip lists

--- linear_skip/search.h
+++ linear_skip/search.h
@@ -25,5 +25,7 @@ struct skiplist_s *express;
 } skiplist_t;
 
 skiplist_t *linear_skip(skiplist_t *list, int value);
+skiplist_t *create_skiplist(int *array, size_t size);
+void free_skiplist(skiplist_t *list);
 
 #endif /* _SEARCH_H_ */
