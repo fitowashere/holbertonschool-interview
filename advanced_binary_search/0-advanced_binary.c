91d86cad645268c388a395cd7cbc21ca251c93b1
fitowashere <fito_washere@icloud.com>
2025-05-06 23:11:44 -0400

    Implement advanced binary search algorithm with recursive approach and add README

---
+++ advanced_binary_search/0-advanced_binary.c
@@ -0,0 +1,82 @@
+#include "search_algos.h"
+#include <stdio.h>
+
+/**
+ * print_array - Prints an array of integers
+ * @array: The array to print
+ * @left: The left index of the array
+ * @right: The right index of the array
+ */
+void print_array(int *array, size_t left, size_t right)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = left; i <= right; i++)
+	{
+		printf("%d", array[i]);
+		if (i < right)
+			printf(", ");
+	}
+	printf("\n");
+}
+
+/**
+ * binary_search_recursive - Recursively searches for a value in a sorted array
+ * @array: Pointer to the first element of the array
+ * @left: The left index of the subarray
+ * @right: The right index of the subarray
+ * @value: The value to search for
+ *
+ * Return: The index where value is located, or -1 if not found
+ */
+int binary_search_recursive(int *array, size_t left, size_t right, int value)
+{
+	size_t mid;
+
+	if (left > right)
+		return (-1);
+
+	/* Print the current subarray */
+	print_array(array, left, right);
+
+	mid = left + (right - left) / 2;
+
+	/* Check if we found the value */
+	if (array[mid] == value)
+	{
+		/* Check if it's the first occurrence or if we need to look further left */
+		if (mid == 0 || array[mid - 1] != value)
+			return (mid);
+		else
+			return (binary_search_recursive(array, left, mid - 1, value));
+	}
+	else if (array[mid] > value)
+	{
+		/* Search in the left subarray */
+		return (binary_search_recursive(array, left, mid - 1, value));
+	}
+	else
+	{
+		/* Search in the right subarray */
+		return (binary_search_recursive(array, mid + 1, right, value));
+	}
+}
+
+/**
+ * advanced_binary - Searches for a value in a sorted array using binary search
+ * @array: Pointer to the first element of the array
+ * @size: Number of elements in the array
+ * @value: The value to search for
+ *
+ * Return: The index where value is located, or -1 if not found
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	/* Check if array is NULL or empty */
+	if (array == NULL || size == 0)
+		return (-1);
+
+	/* Call the recursive binary search function */
+	return (binary_search_recursive(array, 0, size - 1, value));
+}
